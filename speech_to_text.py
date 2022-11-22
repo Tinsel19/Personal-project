7b019762115780189a3178f0d2e54bd1bd3222c6
Tinsel19 <107304041+Tinsel19@users.noreply.github.com>
2022-11-22 06:50:05 +0100

    Speech to text

---
+++ speech_to_text.py
@@ -0,0 +1,45 @@
+
+
+text = input()
+
+dict = {1, 2, 3,4}
+dict.add(8)
+print(dict)
+for x in dict:
+	print(x)
+
+
+#
+
+#import speech_recognition as sr
+#from os import path
+#from pydub import AudioSegment
+#import os
+#import vlc
+#import speech_recognition as sr
+#from pydub import AudioSegment 
+#import subprocess
+#import ffmpeg
+
+#Music = []
+
+#directory = []
+#path = "//storage//emulated//0"
+#for filename in os.listdir(path):
+#	if filename.endswith(".mp3") :
+#		Music.append(os.path.join(path, filename))
+#		
+# convert mp3 file to wav  m
+
+# transcribe audio file                                                         
+
+
+# use the audio file as the audio source
+#audio = sr.AudioFile("Jesse_King_{Buga}(48k).mp3")
+#r = sr.Recognizer()
+#with audio as source:
+#        audio = r.record(source)
+#try:
+#	print("Transcription: " + r.recognize_sphinx(audio))
+#except:
+#	pass
